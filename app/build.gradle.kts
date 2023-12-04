9cd4f4ec9e90004aae8478cf373953b80c1ee4bb
santoshmane <santosh@gitlab>
2023-12-04 22:53:25 +0530

    reusable modifies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.viewbindingdemo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.viewbindingdemo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -30,7 +33,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.viewbindingdemo.ui.theme.ViewBindingDemoTheme
@@ -54,6 +59,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+val reusableModifier = Modifier
+    .fillMaxWidth()
+    .background(Color.Transparent)
+    .padding(12.dp)
+
+// Append to your reusableModifier
+//reusableModifier.clickable { /*...*/ }
+
+// Append your reusableModifier
+//otherModifier.then(reusableModifier)
+
+@Composable
+fun LoadingWheelAnimation() {
+    Box(
+        modifier = reusableModifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -67,6 +95,8 @@ fun ListComposable() {
         "Example", "Android", "Tutorial", "Jetpack", "Compose", "List", "Example", "Simple", "List"
     )
     Column {
+        Spacer(modifier = Modifier.height(20.dp))
+        LoadingWheelAnimation()
         Spacer(modifier = Modifier.height(20.dp))
         HelloContent()
         Spacer(modifier = Modifier.height(20.dp))
@@ -88,7 +118,9 @@ fun ListComposable() {
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(50.dp)
-                        .background(Color.Cyan),
+                        .background(Color.Cyan)
+                        .clip(shape = CircleShape)
+                        .size(50.dp),
                     contentAlignment = Alignment.CenterStart
                 ) {
                     Row {
